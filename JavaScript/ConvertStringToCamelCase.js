12b5fc1c431facbde17e1b71f928edd5119c9b5e
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-04-08 12:18:50 -0300

    Delete ConvertStringToCamelCase.js

--- JavaScript/ConvertStringToCamelCase.js
+++
@@ -1,7 +0,0 @@
-function toCamelCase(str){
-    return str.replace(/[\W+|_](.)/g, function(locale, change){
-        return change.toUpperCase();
-    });
-}
-x = toCamelCase('Teste_to-camel-case');
-console.log(x);
